8e153d962c2d40d458709394c53eb6cb16b574b6
PurityFime <71134621+PurityFime@users.noreply.github.com>
2020-10-25 18:08:20 +0300

    init: upload code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+
+let arr = str.split('');
+if (arr.length % 2 !== 0) {
+    return false;
 }
+else {
+    for (let i = 0; i <= arr.length - 1; i++) {
+        bracketsConfig.forEach( item => {
+          if (arr[i] === item[0] && arr[i+1] === item[1]) {
+            arr.splice(i, 2);
+            i = -1;
+          }
+        })
+    }
+
+
+
+}
+
+return arr.length === 0;
+}
+
+
+
+
